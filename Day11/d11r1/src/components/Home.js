9b59eda3a03d01dce58b92656e842695c92553c3
Jeffrey G <jeffre.g@prodapt.com>
2025-08-07 10:48:24 +0530

    d12c2

--- Day11/d11r1/src/components/Home.js
+++ Day11/d11r1/src/components/Home.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import '../styles/Home.css';
+
 function Home() {
   return (
-    <div>
+    <div className="home-bg">
       <h1>Welcome to the Home Page</h1>
       <p>This is the main entry point of our application.</p>
     </div>
